3e1e6502858b71196dddd707ac555e1e62bf19a0
Sebastian Lievano Arzayus <62860651+SebastianLievano@users.noreply.github.com>
2022-10-17 23:06:27 -0400

    Time: 31 ms (70.00%), Space: 18 MB (64.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        long long int trueSum = 0, arrSum = 0;
+        long long int trueSum = (nums.size()*(nums.size() +1))/2, arrSum = 0;
         for(int i = 0; i < nums.size(); ++i){
-            trueSum += i+1;
+           // trueSum += i+1;
             arrSum += nums[i];
         }
         return (trueSum - arrSum);
