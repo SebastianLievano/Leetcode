cf0b92b16ca6e0a8a96bb9a070144e0044c721c4
Sebastian Lievano Arzayus <62860651+SebastianLievano@users.noreply.github.com>
2022-10-24 16:10:44 -0400

    Time: 243 ms (6.33%), Space: 62.8 MB (5.04%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,49 +1,43 @@
-class Solution { 
-public:
-int DR[4]={1,0,-1,0};
-int DC[4]={0,-1,0,1};
+#define NOTEDGE 3
+#define EDGE -1
+#define VIS_EDGE -2
 
-int numEnclaves(vector<vector<int>>& grid) {
-    int n=grid.size();
-    int m=grid[0].size();
-    queue<pair<int,int>> q;
-    vector<vector<int>> vis(n,vector<int>(m,0));
+class Solution {
+    vector<vector<int>> dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
+    int m, n;
+public:
+    bool inBounds(int i, int j){
+        return !(i < 0 || i > m -1 || j < 0 || j > n-1);
+    }
     
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==0 || i==n-1 || j==0 || j==m-1){
-                if(grid[i][j]==1){
-                    q.push({i,j});
-                    vis[i][j]=1;
-                }
+    void propogateEdge(int i, int j, vector<vector<int>>& g){
+        //if not in bounds or not a new 1
+        if(!inBounds(i,j) || g[i][j] != 1) return;
+        g[i][j] = EDGE;
+        for(auto it:dir){
+            propogateEdge(i + it[0], j + it[1], g);
+        }
+    }
+    
+    
+    int numEnclaves(vector<vector<int>>& g) {
+        m = g.size();
+        n = g[0].size();
+        int numIso = 0;
+        //Changing all lands on edge of board to EDGES
+        for(int i = 0; i < m; ++i){
+            propogateEdge(i, 0, g);
+            propogateEdge(i, n-1, g);
+        }
+        for(int j = 0; j < n; ++j){
+            propogateEdge(0, j, g);
+            propogateEdge(m-1, j, g);
+        }
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(g[i][j] == 1) numIso++;
             }
         }
+        return numIso;
     }
-    
-    while(!q.empty()){
-        int r=q.front().first;
-        int c=q.front().second;
-        
-        q.pop();
-        
-        for(int i=0;i<4;i++){
-            int row=r+DR[i];
-            int col=c+DC[i];
-            
-            if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1 && vis[row][col]==0){
-                q.push({row,col});
-                vis[row][col]=1;
-            }
-        }
-    }
-    
-    int count=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]==1 && vis[i][j]==0)
-                count++;
-        }
-    }
-    return count;
-}
 };
