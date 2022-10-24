8723fc4a2d8744386fe69bb8c0bffadbeb1baa3b
Sebastian Lievano Arzayus <62860651+SebastianLievano@users.noreply.github.com>
2022-10-24 16:09:39 -0400

    Time: 404 ms (5.09%), Space: 62.9 MB (5.04%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,6 +1,6 @@
 #define NOTEDGE 3
 #define EDGE -1
-#define VISITED 2
+#define VIS_EDGE -2
 
 class Solution {
     vector<vector<int>> dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
@@ -10,29 +10,13 @@ public:
         return !(i < 0 || i > m -1 || j < 0 || j > n-1);
     }
     
-    int numIsolated(int i, int j, vector<vector<int>>&g){
-        //If out of bounds or visited or zero, return 0
-        if(!inBounds(i, j) || !g[i][j] || g[i][j] == VISITED) return 0;
-        
-        //If edge, return edge
-        if(g[i][j] == EDGE) return EDGE;
-        
-        int ret, size = 1;
-        
-        //Marking cell as visited
-        g[i][j] = VISITED;
+    void propogateEdge(int i, int j, vector<vector<int>>& g){
+        //if not in bounds or not a new 1
+        if(!inBounds(i,j) || g[i][j] != 1) return;
+        g[i][j] = EDGE;
         for(auto it:dir){
-            //Recursively searching around island
-            ret = numIsolated(i + it[0], j + it[1], g);
-            if(ret == EDGE){
-                //If any adjacent is an edge, setting this island to edge and returning
-                g[i][j] = EDGE;
-                return EDGE;
-            }
-            //If not, adding size of bordering landmass
-            else size += ret;
+            propogateEdge(i + it[0], j + it[1], g);
         }
-        return size;
     }
     
     
@@ -42,21 +26,16 @@ public:
         int numIso = 0;
         //Changing all lands on edge of board to EDGES
         for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                //If grid is a one and on edge, set it as an edge
-                if(g[i][j] &&  (j == 0 || j == n-1|| i == 0 || i == m -1))
-                    g[i][j] = EDGE;
-            }
+            propogateEdge(i, 0, g);
+            propogateEdge(i, n-1, g);
         }
-        int ret;
+        for(int j = 0; j < n; ++j){
+            propogateEdge(0, j, g);
+            propogateEdge(m-1, j, g);
+        }
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
-                //For all ones run search algorithm from their location
-                if(g[i][j] == 1){
-                    ret = numIsolated(i, j, g);
-                    //If search algo does not return as an edge, add size to total
-                    if(ret != EDGE) numIso += ret;
-                }
+                if(g[i][j] == 1) numIso++;
             }
         }
         return numIso;
