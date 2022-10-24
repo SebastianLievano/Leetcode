a24e929a1614b55fdc60267efdabce6343fdf7c2
Sebastian Lievano Arzayus <62860651+SebastianLievano@users.noreply.github.com>
2022-10-24 16:00:50 -0400

    Time: 676 ms (5.09%), Space: 85.5 MB (5.04%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,7 +3,6 @@
 #define VISITED 2
 
 class Solution {
-    vector<vector<int>> g;
     vector<vector<int>> dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
     int m, n;
 public:
@@ -11,7 +10,7 @@ public:
         return !(i < 0 || i > m -1 || j < 0 || j > n-1);
     }
     
-    int numIsolated(int i, int j){
+    int numIsolated(int i, int j, vector<vector<int>>&g){
         //If out of bounds or visited or zero, return 0
         if(!inBounds(i, j) || !g[i][j] || g[i][j] == VISITED) return 0;
         
@@ -24,7 +23,7 @@ public:
         g[i][j] = VISITED;
         for(auto it:dir){
             //Recursively searching around island
-            ret = numIsolated(i + it[0], j + it[1]);
+            ret = numIsolated(i + it[0], j + it[1], g);
             if(ret == EDGE){
                 //If any adjacent is an edge, setting this island to edge and returning
                 g[i][j] = EDGE;
@@ -37,11 +36,10 @@ public:
     }
     
     
-    int numEnclaves(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+    int numEnclaves(vector<vector<int>>& g) {
+        m = g.size();
+        n = g[0].size();
         int numIso = 0;
-        swap(g, grid);
         //Changing all lands on edge of board to EDGES
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
@@ -55,7 +53,7 @@ public:
             for(int j = 0; j < n; ++j){
                 //For all ones run search algorithm from their location
                 if(g[i][j] == 1){
-                    ret = numIsolated(i, j);
+                    ret = numIsolated(i, j, g);
                     //If search algo does not return as an edge, add size to total
                     if(ret != EDGE) numIso += ret;
                 }
